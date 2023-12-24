37406d2ff628a17ba8fd2b7b4d09db2b4f23bcd4
Rex Magana <Rex.magana@gmail.com>
2023-12-23 17:41:55 -0800

    support generic actions

--- tests/derive_action.rs
+++ tests/derive_action.rs
@@ -10,3 +10,30 @@ fn check_macro() {
     let action = MyAction;
     assert_eq!(action.label(), Some("MyLabel"))
 }
+
+#[derive(Debug, Clone, Component, ActionBuilder)]
+#[action_label = "MyGenericLabel"]
+pub struct MyGenericAction<T: Clone + Send + Sync + std::fmt::Debug + 'static> {
+    pub value: T,
+}
+
+#[test]
+fn check_generic_macro() {
+    let action = MyGenericAction { value: 0 };
+    assert_eq!(action.label(), Some("MyGenericLabel"))
+}
+
+#[derive(Debug, Clone, Component, ActionBuilder)]
+#[action_label = "MyGenericWhereLabel"]
+pub struct MyGenericWhereAction<T>
+where
+    T: Clone + Send + Sync + std::fmt::Debug + 'static,
+{
+    pub value: T,
+}
+
+#[test]
+fn check_generic_where_macro() {
+    let action = MyGenericWhereAction { value: 0 };
+    assert_eq!(action.label(), Some("MyGenericWhereLabel"))
+}
