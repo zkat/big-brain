98337faacd5eb720cc820628fd1bfa07e6c8effa
Jacob <biggs.jacob@gmail.com>
2024-04-02 12:10:40 -0500

    feat(scorers): Add support for generic types in ScorerBuilder (#96)

--- tests/derive_scorer.rs
+++ tests/derive_scorer.rs
@@ -12,3 +12,30 @@ fn check_macro() {
     let scorer = MyScorer;
     assert_eq!(scorer.label(), Some("MyLabel"))
 }
+
+#[derive(Debug, Clone, Component, ScorerBuilder)]
+#[scorer_label = "MyGenericLabel"]
+pub struct MyGenericScorer<T: Clone + Send + Sync + std::fmt::Debug + 'static> {
+    pub value: T,
+}
+
+#[test]
+fn check_generic_macro() {
+    let scorer = MyGenericScorer { value: 0 };
+    assert_eq!(scorer.label(), Some("MyGenericLabel"))
+}
+
+#[derive(Debug, Clone, Component, ScorerBuilder)]
+#[scorer_label = "MyGenericWhereLabel"]
+pub struct MyGenericWhereScorer<T>
+where
+    T: Clone + Send + Sync + std::fmt::Debug + 'static,
+{
+    pub value: T,
+}
+
+#[test]
+fn check_generic_where_macro() {
+    let scorer = MyGenericWhereScorer { value: 0 };
+    assert_eq!(scorer.label(), Some("MyGenericWhereLabel"))
+}
